75297a36686284204648b09f772348b50ecfc2fe
Aditya Raj <38121656+AdityaRaj2025@users.noreply.github.com>
2022-08-08 15:10:45 +0900

    Update MainPage.xaml.cs

--- LNT/MainPage.xaml.cs
+++ LNT/MainPage.xaml.cs
@@ -12,16 +12,18 @@ namespace LNT
     public partial class MainPage : ContentPage
     {
        
-        string url = "https://en.wikipedia.org/wiki/Wikipedia";
+         string url = "https://62f096c2a304d71637762981--sunny-bavarois-4f56b7.netlify.app/";
+        MyWebView browser = new MyWebView();
         public MainPage()
         {
             InitializeComponent();
-            var browser = new MyWebView
-            {
-                Source = url,
-            };
-           
+            browser.Source = url;
             webview.Children.Add(browser);
+        }
+         protected override void OnAppearing()
+        {
+            browser.HeightRequest = (stacklayout.Height - stack.Height);
+            base.OnAppearing();
         }
     }
 }
